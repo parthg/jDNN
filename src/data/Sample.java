3847b979f978eb845549150b2efc6c912a92dfbf
parthg <parthg.88@gmail.com>
2014-12-04 18:03:54 +0100

    configured with serialversionUID to be user-defined

--- src/data/Sample.java
+++ src/data/Sample.java
@@ -4,9 +4,11 @@ import java.util.Set;
 import java.util.HashSet;
 import java.io.Serializable;
 
-public class Sample implements Serializable { 
+public class Sample implements Serializable {
+  static final long serialVersionUID = 42L; 
   Set<String> pos;
   Set<String> neg;
+  
   public Sample() {
     this.pos = new HashSet<String>();
     this.neg = new HashSet<String>();
@@ -19,9 +21,19 @@ public class Sample implements Serializable {
   public void addNeg(String txt) {
     this.neg.add(txt);
   }
+
+  public Set<String> getPos() {
+    return this.pos;
+  }
+
+  public Set<String> getNeg() {
+    return this.neg;
+  }
+
   public int getPosSize() {
     return this.pos.size();
   }
+  
   public int getNegSize() {
     return this.neg.size();
   }
